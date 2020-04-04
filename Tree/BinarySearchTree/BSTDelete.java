4871b9bfbfb5272a9d0b24df5047445cc541440e
Rishabh Sharma <15ucc030@lnmiit.ac.in>
2020-04-05 03:53:34 +0530

    4/5/2020

--- Tree/BinarySearchTree/BSTDelete.java
+++ Tree/BinarySearchTree/BSTDelete.java
@@ -20,35 +20,103 @@ public class BSTDelete{
     //Initialising header
     static Node root;
     
-    //Inserting a node 
-    static Node insertBST(Node root, Node newNode){
+    //Tree Inorder
+    static void treeInorder(Node root){
+        //Base condition
         if(root == null){
-            root = newNode;
-            return root;
-        }
+            return;
+        } 
 
-        if(root.key > newNode.key){
-            root.left = insertBST(root.left, newNode);
-        }else{
-            root.right = insertBST(root.right, newNode);
-        }
-        return root;
+        treeInorder(root.left);
+        System.out.println(root.key);
+        treeInorder(root.right);
     }
 
     ///////////////////////// DELETING A NODE IN BST AND BALENCING THE TREE AGAIN ///////////////////////////////
+    static Node deleteNodeBST(Node root, int key){
+        if(root == null){
+            return root;
+        }
+
+        if(key < root.key){
+            root.left = deleteNodeBST(root.left, key);
+        }else if(key > root.key){
+            root.right = deleteNodeBST(root.right, key);
+        }else{
+            //This case will handle if Current node key is the key which we want to delete
 
 
+            //These if else  handles if Node to be deleted has one child then that child replace the node deleted
+            //This also handles the case when leef node is to be deleted here null is retured to root.left or root.right
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+
+            //Case if a node has two children
+            //We replace the node by Inorder Successor of that node(Smallest in the right sub tree)
+            root.key = minVal(root.right);
+            
+            //Now we will delete that key from the right subtree
+            root.right = deleteNodeBST(root.right, root.key);
+        }
+       return root;
+    }
+
+    static int minVal(Node root){
+        int key = root.key;
+        while(root.left != null){
+            key = root.left.key;
+            root = root.left;
+        }
+        return key;
+    }
     
+    // This method mainly calls insertRec() 
+    static void insert(int key) 
+    { 
+        root = insertRec(root, key); 
+    } 
+
+    static Node insertRec(Node root, int key) 
+    { 
+  
+        /* If the tree is empty, return a new node */
+        if (root == null) 
+        { 
+            root = new Node(key); 
+            return root; 
+        } 
+  
+        /* Otherwise, recur down the tree */
+        if (key < root.key) 
+            root.left = insertRec(root.left, key); 
+        else if (key > root.key) 
+            root.right = insertRec(root.right, key); 
+  
+        /* return the (unchanged) node pointer */
+        return root; 
+    } 
 
 
     public static void main(String args[]){
-        root = new Node(4);
-        root.left = new Node(2);
-        root.left.left = new Node(1);
-        root.left.right = new Node(3);
-        root.right = new Node(5);
+        root = new Node(50);
+        insert(30); 
+        insert(20); 
+        insert(40); 
+        insert(70); 
+        insert(60); 
+        insert(80); 
 
+        //Printing whole tree
+        treeInorder(root);
 
+        //Deleting leaf node
+        deleteNodeBST(root, 20);
+
+        //Updated Tree
+        treeInorder(root);
     }
 
 }
